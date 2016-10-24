a2ef20fee7b2ec1bcc36c56e52e89f6f6a73596f
davestroud <david@davidstroud.me>
2016-10-24 09:14:14 -0500

    add ParadiseInfoJDS

--- Solution Files/Chapter02/Solutions/YouDoIt/Test.java
+++ Solution Files/Chapter02/Solutions/YouDoIt/Test.java
@@ -2,9 +2,15 @@ import javax.swing.JOptionPane;
 public class Test
 {
   public static void main(String[] args) {
-    int creditDays = 30;
-    JOptionPane.showMessageDialog(null, "" + creditDays);
-    JOptionPane.showMessageDialog
-          (null, "Every bill is due in " + creditDays + " days");
+    int anInt = 1234;
+    byte aByte = 12;
+    short aShort = 12345;
+    long aLong = 12344321123413545L;
+    int anotherInt = anInt * 1000;
+    System.out.println("The int is " + anInt);
+    System.out.println("The byte is " + aByte);
+    System.out.println("The short is " + aShort);
+    System.out.println("The long is " + aLong);
+    System.out.println("Another int is " + anotherInt);
   }
 }
---
+++ Solution Files/Chapter03/Solutions/YouDoIt/ParadiseInfoJDS.java
@@ -0,0 +1,11 @@
+public class ParadiseInfoJDS
+{
+  public static void main(String[] args) {
+    displayInfo();
+  }
+  public static void displayInfo()
+  {
+    System.out.println("Paradise Day Spa wants to pamper you.");
+    System.out.println("We will make look good.");
+  }
+}
