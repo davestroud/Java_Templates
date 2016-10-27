07e7c460627208a8dc0ce13ee232115e344893fd
davestroud <david@davidstroud.me>
2016-10-27 17:24:18 -0500

    add Homework 2 file

---
+++ Homework2_Stroud/P2_Stroud_JOptionPane.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;  // program uses JOptionPane class
+public class P2_Stroud_JOptionPane {
+
+  public static void main(String[] args) {
+
+    int sum, average, product, smallest, largest;
+    int num1, num2, num3;
+
+  }
+
+    public static int sum(int num1, int num2, int num3) {
+      sum = num1 + num2 + num3;
+    }
+
+    public static int average(int num1, int num2, int num3) {
+      average = (num1 + num2 + num3) / 3;
+    }
+
+    public static int product() {
+      product = num1 * num2 * num3;
+    }
+
+    public static int smallest() {
+
+    }
+
+    public static int largest() {
+
+    }
+}
--- Solution Files/Chapter03/Solutions/ProgrammingExercises/Ex03-08/Insurance.java
+++ Solution Files/Chapter03/Solutions/ProgrammingExercises/Ex03-08/Insurance.java
@@ -1,14 +1,14 @@
-
 import java.util.Scanner;
+
 public class Insurance {
 	private static Scanner input;
 
-	public static void main (String args[]) {
+	public static void main(String args[]) {
 		int currYear;
 		int birthYear;
 		int premium;
 		input = new Scanner(System.in);
-		System.out.println("Enter the current year >> " );
+		System.out.println("Enter the current year >> ");
 		currYear = input.nextInt();
 		System.out.print("Enter the birth year >>");
 		birthYear = input.nextInt();
@@ -16,12 +16,13 @@ public class Insurance {
 		System.out.println("The premium is $" + premium);
 	}
 
-public static int calculatePremium(int curr, int birth) {
-	final int ADDITION_FACTOR = 15;
-	final int MULTIPLICATION_FACTOR = 20;
-	int age = curr - birth;
-	int decade = age / 10;
-	int premium = (ADDITION_FACTOR + decade) * MULTIPLICATION_FACTOR;
-	return premium;
+	public static int calculatePremium(int curr, int birth) {
+		final int ADDITION_FACTOR = 15;
+		final int MULTIPLICATION_FACTOR = 20;
+		int age = curr - birth;
+		int decade = age / 10;
+		int premium = (ADDITION_FACTOR + decade) * MULTIPLICATION_FACTOR;
+		return premium;
 	}
 }
+ 
