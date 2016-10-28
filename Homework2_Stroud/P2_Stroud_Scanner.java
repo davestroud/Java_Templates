ece519703e7ff61645304bd077b60972d25f50ff
davestroud <david@davidstroud.me>
2016-10-28 09:23:29 -0500

    update methods on homework 2

--- Homework2_Stroud/P2_Stroud_Scanner.java
+++ Homework2_Stroud/P2_Stroud_Scanner.java
@@ -3,23 +3,28 @@ public class P2_Stroud_Scanner {
 
   public static void main(String[] args) {
 
-    Scanner kb = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     int num1, num2, num3;
-    System.out.print("Enter an integer >> ");
-    num1 = kb.nextInt();
-    System.out.print("Enter another integer >> ");
-    num2 = kb.nextInt();
-    System.out.print("Enter another integer >> ");
-    num3 = kb.nextInt();
+    System.out.print("Enter first integer >> ");
+    num1 = input.nextInt();
+    System.out.print("Enter second integer >> ");
+    num2 = input.nextInt();
+    System.out.print("Enter third integer >> ");
+    num3 = input.nextInt();
     displaySum(num1, num2, num3);
     displayAverage(num1, num2, num3);
+    displayProduct(num1, num2, num3);
   }
 
   public static void displaySum(int x, int y, int z) {
-    System.out.println(x + y + z);
+    System.out.println("Sum: " + (x + y + z));
   }
 
   public static void displayAverage(int x, int y, int z) {
-    System.out.println((x + y + z) / 3);
+    System.out.println("Average: " + ((x + y + z) / 3));
+  }
+
+  public static void displayProduct(int x, int y, int z) {
+    System.out.println("Product" + (x * y * z));
   }
 }
