67a7efac21dfe6d20d7410251f6bb9d38f568f46
davestroud <david@davidstroud.me>
2016-11-10 14:53:04 -0600

    complete Average of Quizzes file

--- Arrays/AverageOfQuizzes.java
+++ Arrays/AverageOfQuizzes.java
@@ -4,6 +4,7 @@ public class AverageOfQuizzes {
 
   public static void main(String[] args) {
 
+    int[] scores = new int[10];
     int score = 0;
     int count = 0;
     int total = 0;
@@ -11,7 +12,7 @@ public class AverageOfQuizzes {
     final int MAX = 10;
     Scanner input = new Scanner(System.in);
     System.out.print("Enter a quiz score or " +
-        QUIT + "to quit   >> ");
+        QUIT + " to quit   >> ");
     score = input.nextInt();
     while(score != QUIT) {
 
@@ -23,8 +24,15 @@ public class AverageOfQuizzes {
       else {
         System.out.print("Enter next quiz score or " +
             QUIT + " to quit >> ");
-        score = input + input.nextInt();
+        score = input.nextInt();
       }
     }
+    System.out.print("\nThe scores entered were: ");
+    for(int x = 0; x < count; ++x)
+        System.out.print(scores[x] + " ");
+    if(count != 0)
+        System.out.println("\n The average is " + (total * 1.0 / count));
+    else
+        System.out.println("No scores were ented.");
   }
 }
