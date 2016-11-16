3b2819fc7cbd1a6e992a1af094fac2b21d17464c
davestroud <david@davidstroud.me>
2016-11-15 19:58:16 -0600

    create subclass objects in PayrollSystemTest class

---
+++ Employee_Payroll/PayrollSystemTest.java
@@ -0,0 +1,33 @@
+// PayrollSystemTest.java
+// Employee hierarchy test program
+
+public class PayrollSystemTest {
+
+  public static void main(String[] args) {
+
+    // Create subclass objects
+    SalariedEmployee salariedEmployee =
+      new SalariedEmployee hourlyEmployee("John", "Smith", "111-11-111", 800.00 );
+    HourlyEmployee hourlyEmployee =
+      new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75, 40 );
+    CommissionEmployee commissionEmployee =
+      new CommissionEmployee("Sue", "Jones", "333-33-333", 10000, .06);
+    BasePlusCommissionEmployee basePlusCommissionEmployee =
+      new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000, .04, 300 );
+
+    System.out.println("Employees processed individually:\n" );
+
+    System.out.printf( "%s\n%s: $%,.2f\n\n",
+       salariedEmployee, "earned", salariedEmployee.earnings() );
+    System.out.printf( "%s\n%s: $%,.2f\n\n",
+       hourlyEmployee, "earned", hourlyEmployee.earnings() );
+    System.out.printf( "%s\n%s: $%,.2f\n\n",
+       commissionEmployee, "earned", commissionEmployee.earnings() );
+    System.out.printf( "%s\n%s: $%,.2f\n\n",
+    basePlusCommissionEmployee,
+    "earned", basePlusCommissionEmployee.earnings() );
+
+
+
+  }
+}
