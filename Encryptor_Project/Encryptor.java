4c2f3c1ce64201c735bf70c93ccb03698953133a
davestroud <david@davidstroud.me>
2017-01-06 20:18:11 -0500

    add Solution Files

--- Encryptor_Project/Encryptor.java
+++ Encryptor_Project/Encryptor.java
@@ -8,25 +8,6 @@ import static java.nio.file.AccessMode.*;
 public class Encryptor {
 
   public static void main(String[] args) {
-
-    Path keywordFile =
-      Paths.get("//Users//davidstroud//Java_Templates//Encryptor_Project//keyword.txt");
-    Path inputFile =
-      Paths.get("//Users//davidstroud//Java_Templates//Encryptor_Project//input.txt");
-    byte[] data = s.getBytes();
-
-    OutputStream output = null;
-    try
-    {
-      output = new BufferedOutputStream(Files.newOutputStream(keywordFile, READ));
-      output = new BufferedOutputStream(Files.newOutputStream(inputFile, READ));
-      output.write(data);
-      output.flush();
-      output.close();
-    }
-    catch(Exception e)
-    {
-      System.out.println("Message: " + e);
-    }
+    
   }
 }
--- Encryptor_Project/EncryptorTester.java
+++ Encryptor_Project/EncryptorTester.java
@@ -1,6 +1,32 @@
-// Handle all the file input and output and pass
-// and receive text from the Encrypter class
-
 import java.nio.file.*;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.AccessMode.*;
 
-public class EncryptorTester
+public class EncryptorTester {
+
+  public static void main(String[] args) {
+
+    Path keywordFile =
+      Paths.get("//Users//davidstroud//Java_Templates//Encryptor_Project//keyword.txt");
+    Path inputFile =
+      Paths.get("//Users//davidstroud//Java_Templates//Encryptor_Project//input.txt");
+    byte[] data = s.getBytes();
+
+    OutputStream output = null;
+    try
+    {
+      output = new BufferedOutputStream(Files.newOutputStream(keywordFile, READ));
+      output = new BufferedOutputStream(Files.newOutputStream(inputFile, READ));
+      output.write(data);
+      output.flush();
+      output.close();
+    }
+    catch(Exception e)
+    {
+      System.out.println("Message: " + e);
+    }
+  }
+}
