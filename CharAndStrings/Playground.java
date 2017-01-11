3848fce1d2ff297a6d8f34c3c96aa082932d0e09
David Stroud <david@greymatter-ag.com>
2017-01-11 15:36:43 -0500

    add Playground to String folder

---
+++ CharAndStrings/Playground.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Playground {
+  public static void main(String[] args) {
+    String greeting = "Hello";
+    int len = greeting.length();
+    System.out.println(len);
+
+    String myName = "David";
+    int len1 = myName.length();
+    System.out.println(len1);
+    System.out.println (myName.indexOf('a'));
+    System.out.println(myName.toUpperCase());
+    System.out.println(myName.toLowerCase());
+
+  }
+}
